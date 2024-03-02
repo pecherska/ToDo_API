ccba3649d66b6c99816b875c511b1ff8f5660fdc
Nina Pecherska <npecherska@gmail.com>
2024-03-02 17:48:10 +0200

    delete

--- src/routes/auth.js
+++
@@ -1,8 +0,0 @@
-// import express from "express";
-// import { googleAuth, googleRedirect } from "./auth.js";
-
-// const router = express.Router();
-
-// router.get("/google", tryCatchWrapper(googleAuth));
-
-// router.get("/google-redirect", tryCatchWrapper(googleRedirect));
